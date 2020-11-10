7a105cca894eabbd2a8bb3a4a64ae1a64f44394e
Butcher, Arthur W (UG - Computer Science) <ab02038@surrey.ac.uk>
2020-11-10 17:44:30 +0000

    Creation of Entry Model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_10_174143) do
+ActiveRecord::Schema.define(version: 2020_11_10_174339) do
 
   create_table "drivers", force: :cascade do |t|
     t.string "name"
@@ -21,6 +21,15 @@ ActiveRecord::Schema.define(version: 2020_11_10_174143) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["teams_id"], name: "index_drivers_on_teams_id"
+  end
+
+  create_table "entries", force: :cascade do |t|
+    t.integer "team_id"
+    t.integer "race_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["race_id"], name: "index_entries_on_race_id"
+    t.index ["team_id"], name: "index_entries_on_team_id"
   end
 
   create_table "positions", force: :cascade do |t|
