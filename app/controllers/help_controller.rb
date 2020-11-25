f3425a5947a49d7501a88258bc21ece551216c7b
Butcher, Arthur W (UG - Computer Science) <ab02038@surrey.ac.uk>
2020-11-25 19:09:47 +0000

    Help page created. Gmail address created to send emails (not required but wanted to give it a go.

---
+++ app/controllers/help_controller.rb
@@ -0,0 +1,21 @@
+class HelpController < ApplicationController
+  def index
+    @contact = Help.new()
+  end
+
+  def create
+    @contact = Help.new()
+    @contact.request = request
+    respond_to do |format|
+      if @contact.deliver
+        # re-initialize Home object for cleared form
+        @contact = Help.new
+        format.html { render 'index'}
+        format.js   { flash.now[:success] = @message = "Thank you for your message. I'll get back to you soon!" }
+      else
+        format.html { render 'index' }
+        format.js   { flash.now[:error] = @message = "Message did not send." }
+      end
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,16 @@
 Rails.application.routes.draw do
+  get 'help/index'
   get 'home', to: 'site#home'
   get 'tracks', to: 'tracks#index'
   get 'drivers', to: 'drivers#index'
   get 'teams', to: 'teams#index'
+  get 'help', to: 'help#index'
   resources :entries
   resources :positions
   resources :races
   resources :tracks
   resources :drivers
   resources :teams
+  resources :help
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
